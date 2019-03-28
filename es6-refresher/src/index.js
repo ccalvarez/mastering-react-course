e35e1499791d5486abac675145df01f43e0aa4bc
Carolina Cordero <ccalvarez@gmail.com>
2019-03-28 13:41:30 -0600

    2-8 Array.map Method

--- es6-refresher/src/index.js
+++ es6-refresher/src/index.js
@@ -1,45 +1,4 @@
-// arrow functions don't rebind this
+const colors = ['red', 'green', 'blue'];
 
-// this references to the person object:
-// const person = {
-//   name: 'Caro',
-//   talk() {
-//     console.log('this', this);
-//   },
-// };
-
-// log this inside a callback function
-// the callback function is not part of any object
-// it's a stand alone function
-// const person = {
-//   name: 'Caro',
-//   talk() {
-//     setTimeout(function() {
-//       console.log('this', this);
-//     }, 1000);
-//   },
-// };
-
-// how to print the reference to the person object?
-// #1: pattern that we used in the old days:
-// const person = {
-//   name: 'Caro',
-//   talk() {
-//     var self = this;
-//     setTimeout(function() {
-//       console.log('self', self);
-//     }, 1000);
-//   },
-// };
-
-// how to print the reference to the person object?
-// #2: arrow function don't rebind this,
-// arrow function will inherit the this keyword:
-const person = {
-  name: 'Caro',
-  talk() {
-    setTimeout(() => console.log('this', this), 1000);
-  },
-};
-
-person.talk();
+const items = colors.map(color => `<li>${color}</li>`);
+console.log(items);

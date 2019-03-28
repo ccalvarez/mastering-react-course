0e0a54c524943265a79a78a87ffda79f253b0302
Carolina Cordero <ccalvarez@gmail.com>
2019-03-28 14:02:10 -0600

    2-10 Spread Operator

--- es6-refresher/src/index.js
+++ es6-refresher/src/index.js
@@ -1,14 +1,28 @@
-const address = {
-  street: '2167 Strasse',
-  city: 'Berlin',
-  country: 'Germany',
-};
+// Spread Operator
+// const first = [1, 2, 3];
+// const second = [4, 5, 6];
 
-// const street = address.street;
-// const city = address.city;
-// const country = address.country;
+// Combine arrays:
+// #1: The old way:
+// const combined = first.concat(second);
 
-// const { street, city, country } = address;
-const { street: st, city, country } = address;
+// #2: With spread operator:
+// const combined = [...first, 'a', ...second, 'b'];
+// console.log(combined);
 
-console.log(`Street: ${st}, City: ${city}, Country: ${country}`);
+// Clone the first array:
+// const clone = [...first];
+// console.log(first);
+// console.log(clone);
+
+// Spread operator on objects:
+// #1: Combine two objects:
+const first = { name: 'Mosh' };
+const second = { job: 'Instructor' };
+// Combine these two objects in one object:
+const combined = { ...first, ...second, location: 'Australia' };
+console.log('Combined object', combined);
+
+// #2: Clone the first object:
+const clone = { ...first };
+console.log('Cloned object', clone);

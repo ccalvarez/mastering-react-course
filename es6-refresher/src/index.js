465c00e04e303d9e1e61c065bd79c429a4aab68b
Carolina Cordero <ccalvarez@gmail.com>
2019-03-28 13:49:50 -0600

    2-10 Object Destructuring

--- es6-refresher/src/index.js
+++ es6-refresher/src/index.js
@@ -1,4 +1,14 @@
-const colors = ['red', 'green', 'blue'];
+const address = {
+  street: '2167 Strasse',
+  city: 'Berlin',
+  country: 'Germany',
+};
 
-const items = colors.map(color => `<li>${color}</li>`);
-console.log(items);
+// const street = address.street;
+// const city = address.city;
+// const country = address.country;
+
+// const { street, city, country } = address;
+const { street: st, city, country } = address;
+
+console.log(`Street: ${st}, City: ${city}, Country: ${country}`);

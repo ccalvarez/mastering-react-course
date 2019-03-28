6be8865993200e3978cec60f0de31646e0d95bca
Carolina Cordero <ccalvarez@gmail.com>
2019-03-28 13:08:27 -0600

    2-7 Arrow Functions and this

--- es6-refresher/src/index.js
+++ es6-refresher/src/index.js
@@ -1,22 +1,45 @@
-// const square = function(number) {
-//   return number * number;
+// arrow functions don't rebind this
+
+// this references to the person object:
+// const person = {
+//   name: 'Caro',
+//   talk() {
+//     console.log('this', this);
+//   },
 // };
 
-const square = number => number * number;
-console.log(square(5));
+// log this inside a callback function
+// the callback function is not part of any object
+// it's a stand alone function
+// const person = {
+//   name: 'Caro',
+//   talk() {
+//     setTimeout(function() {
+//       console.log('this', this);
+//     }, 1000);
+//   },
+// };
 
-// application:
-const jobs = [
-  { id: 1, isActive: true },
-  { id: 2, isActive: true },
-  { id: 3, isActive: false },
-];
+// how to print the reference to the person object?
+// #1: pattern that we used in the old days:
+// const person = {
+//   name: 'Caro',
+//   talk() {
+//     var self = this;
+//     setTimeout(function() {
+//       console.log('self', self);
+//     }, 1000);
+//   },
+// };
 
-// console.log(
-//   jobs.filter(function(job) {
-//     return job.isActive;
-//   })
-// );
-console.log(jobs.filter(job => job.isActive));
+// how to print the reference to the person object?
+// #2: arrow function don't rebind this,
+// arrow function will inherit the this keyword:
+const person = {
+  name: 'Caro',
+  talk() {
+    setTimeout(() => console.log('this', this), 1000);
+  },
+};
 
-
+person.talk();

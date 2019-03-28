f3976b3fd2b5cf9613c2e93c43b9760ec7541f24
Carolina Cordero <ccalvarez@gmail.com>
2019-03-28 15:45:01 -0600

    2-14 Named and Default Exports

--- es6-refresher/src/index.js
+++ es6-refresher/src/index.js
@@ -1,4 +1,11 @@
-import { Teacher } from './teacher';
+import Teacher, { promote } from './teacher';
+
+// Default export -> import ... from '';
+// Named export -> import { ... } from '';
+
+// Mismo patrón que en:
+// import React, { Component } from 'react';
 
 const teacher = new Teacher('Mosh', 'MSc');
 teacher.teach();
+promote();
--- es6-refresher/src/teacher.js
+++ es6-refresher/src/teacher.js
@@ -1,6 +1,10 @@
 import { Person } from './person';
 
-export class Teacher extends Person {
+// Named export:
+export function promote() {}
+
+// Default export:
+export default class Teacher extends Person {
   constructor(name, degree) {
     super(name);
     this.degree = degree;
